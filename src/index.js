3fb4e55172c9569dc852c0d91b4a0a6d5218ef99
frandev <franciscoguevara03@gmail.com>
2024-04-19 20:32:10 -0400

    add: configuracion del puerto para la API

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ import tokenRoutes from './routes/token.routes.js'
 import 'dotenv/config'
 
 const app = express()
-const puerto = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Bien venidos API-BCV')
+  res.send('Bienvenidos API-BCV')
 })
 
 app.use("/api", tokenRoutes)
 app.use("/api", userRoutes)
 app.use("/api", rolesRoutes)
 
-puerto ? app.listen(puerto) : app.listen(3000)
-console.log('Server on Port', process.env.PORT)
+app.listen(port, () => {
+  console.log(`API-BCV listening on port ${port}!`)
+})
